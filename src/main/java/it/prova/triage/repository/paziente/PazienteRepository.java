c0cb69bf53a49b4d6f6d4b1c2a3ae7b16e84c925
MattiaLR95 <laroccamattia@virgilio.it>
2022-12-04 13:36:40 +0100

    Creazione dei repository

---
+++ src/main/java/it/prova/triage/repository/paziente/PazienteRepository.java
@@ -0,0 +1,17 @@
+package it.prova.triage.repository.paziente;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.triage.model.Paziente;
+
+public interface PazienteRepository extends CrudRepository<Paziente, Long> {
+
+	Optional<Paziente> findByCodiceFiscale(String codiceFiscaleInput);
+
+	@Query("select p from Paziente p where p.codiceDottore=?1")
+	Paziente findPazienteByCodiceDottore(String codiceDottoreInput);
+
+}
